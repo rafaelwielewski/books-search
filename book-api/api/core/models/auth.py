86c96dbee1b5b4a6a5ea82c36fdc77bb25ba5e26
Tadeu Jenuario <44622499+Gnoario@users.noreply.github.com>
2025-07-26 17:25:10 -0300

    feat: add auth requirement

---
+++ book-api/api/core/models/auth.py
@@ -0,0 +1,13 @@
+from typing import Optional
+from pydantic import BaseModel
+
+class LoginRequest(BaseModel):
+    username: str
+    password: str
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
